f7273dfcdabaabd41ddd380f5ecec5fe59d7f649
riku89-2004 <99306142+riku89-2004@users.noreply.github.com>
2025-01-09 01:29:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -193,4 +193,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # 環境変数PORTを取得し、デフォルト値5000を使用
+    app.run(host="0.0.0.0", port=port)
